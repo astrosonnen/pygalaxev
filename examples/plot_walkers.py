ea1db7fb51e86834072ca0cebc7a097210012f89
Naadiyah <54985487+Naadiyah@users.noreply.github.com>
2020-09-24 10:51:13 +0200

    Update plot_walkers.py

--- examples/plot_walkers.py
+++ examples/plot_walkers.py
@@ -38,7 +38,7 @@ fig2, axes2 = plt.subplots(ncols=1, nrows=len(mags))
 fig2.set_size_inches(12,8)
 
 for j in np.arange(len(chain)-len(mags), len(chain)):
-	ax = j- len(mags)-2
+	ax = j - len(mags) - 1
 	for i in range(nr_walkers):
 		axes2[ax].plot(chain[pars[j]][i,:], color='black', alpha=0.3)
 		axes2[ax].set_ylabel(pars[j])
